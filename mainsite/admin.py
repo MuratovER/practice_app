a7b850398b70b188a2e5c4bcadaf49df4376a3ce
Eldar <MuratovER@stud.kai.ru>
2022-02-14 14:33:12 +0300

    Created Euler problems page

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from mainsite.models import Post, Comment, CodeExamples
+from mainsite.models import Post, Comment, CodeExamples, EulerProblem
 
 admin.site.register(Comment)
 admin.site.register(Post)
 admin.site.register(CodeExamples)
+admin.site.register(EulerProblem)
