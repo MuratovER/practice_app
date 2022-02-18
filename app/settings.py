101b72ada74644f6f459f25846df252feab93fd8
Eldar <MuratovER@stud.kai.ru>
2022-02-18 21:44:40 +0300

    Prepared project for deploying to heroku

--- app/settings.py
+++ app/settings.py
@@ -141,9 +141,7 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
