997dc738ebf6d9d7e12b9674f455e49f25da6e81
Eldar <MuratovER@stud.kai.ru>
2022-02-19 13:14:58 +0300

    Created layout for code examples

---
+++ mainsite/static/mainsite/js/euler_problem.js
@@ -0,0 +1,13 @@
+// var a;
+// function show_hide()
+// {
+//     if(a==1)
+//     {
+//         document.getElementsByClassName("ler-problems").style.display="inline";
+//         return a=0;
+//     }
+//     else {
+//         document.getElementsByClassName("hidden-euler-problems").style.display="none";
+//         return a=1;
+//     }
+// }
